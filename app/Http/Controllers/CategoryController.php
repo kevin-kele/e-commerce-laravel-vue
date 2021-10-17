c4461648061cf50416d80bea2c4b409ee8276dbc
kevin-kele <kevinkele1@gmail.com>
2021-10-17 03:28:12 +0200

    relation entre tb

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,15 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CategoryController extends Controller
 {
     //
     public function AllCat(){
-        return view(
-            'admin.category.index'
-        );
+        $categories = Category::latest()->paginate(5);
+        return view('admin.category.index',compact('categories'));
     }
 
+
+    public function AddCat(Request $request){
+        $validatedData = $request->validate([
+            'category_name' => 'required|unique:categories|max:255',
+        ],[
+            'category_name.required'=>'veuillez entre une categories'
+        ]);
+        Category::insert([
+            'category_name'=>$request->category_name,
+            'user_id'=>Auth::user()->id,
+            'created_at'=>Carbon::now()
+        ]);
+        // $category = new Category;
+        // $category->category_name = $request->category_name;
+        // $category->user_id =Auth::user()->id;
+        // $category->save();
+        return redirect()->back()->with('success','Category inserted successfull !');
+    }
 }
--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -17,31 +17,56 @@
             <div class="row">
                 <div class="col-md-8">
                     <div class="card">
+                        @if(session('success'))
+                            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            <strong>{{session('success')}}</strong>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                            </div>
+                        @endif
                         <div class="card-header">All Category</div>
                         <table class="table">
                                 <thead>
                                     <tr>
-                                    <th scope="col">id</th>
-                                    <th scope="col">name</th>
-                                    <th scope="col">email</th>
-                                    <th scope="col">created</th>
+                                    <th scope="col">Sl No</th>
+                                    <th scope="col">Category name</th>
+                                    <th scope="col">User</th>
+                                    <th scope="col">Created</th>
                                     </tr>
-                                </thead>
+                                    </thead>
 
+                                @foreach($categories as $categorie)
                                 <tbody>
                                     <tr>
-                                    <th scope="row"></th>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
+                                    <th scope="row">{{$categories->firstItem()+$loop->index}}</th>
+                                    <td>{{$categorie->category_name}} </td>
+                                    <td>{{$categorie->user->name}} </td>
+                                    <td>{{$categorie->created_at->diffForHumans()}}</td>
                                     </tr>
                                 </tbody>
+                                @endforeach
                         </table>
+                        {{$categories->links()}}
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="card">
                         <div class="card-header">Add Category</div>
+                        <div class="card-body">
+                            <form action="{{ route('store.category') }}" method="POST">
+                                @csrf
+                                <div class="form-group ">
+                                    <label for="inputName">Category Name</label>
+                                    <div class="col-sm-1-12">
+                                        <input type="text" class="form-control" name="category_name" id="inputName" placeholder="">
+                                        @error('category_name')
+                                            <span class="text-danger">{{$message}}</span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                 <button type="submit" class="btn btn-primary mt-2">Add category</button>
+                            </form>
+                        </div>
+
                     </div>
                 </div>
             </div>
